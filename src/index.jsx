355ab3fad52940e89e08a431c72dd89daccddafa
Larry Bucio <larry.bucio@gmail.com>
2023-11-18 18:11:45 -0700

    Update styling and format

--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,51 @@
 // Import React and ReactDOM libraries
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 // Define a custom component that fetches and displays the subscriber count
-class SubscriberCount extends React.Component {
-    // Initialize the state with a default value
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 'Loading...'
-        };
-    }
-
-    // Fetch the subscriber count from the YouTube API when the component mounts
-    componentDidMount() {
-        const channelId = 'UCxJXJmk3H3H3fxYiCr3VD8A';
-        const apiKey = process.env.REACT_APP_API_KEY;
+const SubscriberCount = () => {
+    const [count, setCount] = useState('Loading...')
+    const [videoCount, setVideoCount] = useState('')
+    const [viewCount, setViewCount] = useState('')
+    const channelId = 'UCxJXJmk3H3H3fxYiCr3VD8A';
+    const apiKey = process.env.REACT_APP_API_KEY;
+    
+    useEffect(() => {
         const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`;
         fetch(url)
             .then(response => response.json()) // Parse the JSON response
             .then(data => {
                 // Extract the subscriber count from the data
-                const count = data.items[0].statistics.subscriberCount;
+                const statistics = data.items[0].statistics;
+                const count = statistics.subscriberCount;
+                const { videoCount } = statistics;
+                const { viewCount } = statistics;
                 // Update the state with the count
-                this.setState({ count });
+                setCount(count);
+                setVideoCount(videoCount)
+                setViewCount(viewCount)
             })
             .catch(error => {
                 // Handle any errors
                 console.error(error);
                 // Update the state with an error message
-                this.setState({ count: 'Error' });
+                setCount('Error');
             });
-    }
+    }, [apiKey])
 
-    // Render the subscriber count as a white bold text
-    render() {
-        return (
+    return (
+        <>
             <h1 style={{ color: 'white', fontWeight: 'bold' }}>
-                {this.state.count}
+                {count}
             </h1>
-        );
-    }
+            <h2>Subscribers</h2>
+            <div className='sub-stats'>
+                <div>Videos: {videoCount}</div>
+                <div>Views: {viewCount}</div>
+            </div>
+        </>
+    );
 }
 
 // Define the main app component that renders the subscriber count component
